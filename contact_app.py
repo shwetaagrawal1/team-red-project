80ba76c06ffa320a0deca0bf0d5ba51630bc31af
adarshnamsani <adarsh.namsani@gmail.com>
2016-07-14 16:32:36 +0530

    "modified page of contact_app"@team_red#adarsh

--- contact_app.py
+++ contact_app.py
@@ -1,13 +1,30 @@
 import sys
 import json
+from json_reader import JSONReader
+from Person import Person
+from directory import Directory
 
 class JSONWriter:
     def __init__(self):
         pass
-    def add_contact(self,person):
-        dict = {person.get_phone_number():{"name":person.get_name(),"email":person.get_email_id(),"city":person.get_city()}}
 
-        with open("jsonFile.json","r") as filepointer:
+    temp_dict = {}
+    get_obj = JSONReader()
+    data = get_obj.get_all()
+    for recordes in data:
+        temp_dict.update({recordes.get_phone_number(): {"name": recordes.get_name(), "email": recordes.get_email_id(),"city": recordes.get_city()}})
+
+    def add_contact(self,person):
+
+        dict = {person.get_phone_number():{"name":person.get_name(),"email":person.get_email_id(),"city":person.get_city()}}
+        self.temp_dict.update(dict)
+
+        with open("jsonFile.json", "w") as filepointer:
+            filepointer.write(json.dumps(self.temp_dict))
+
+
+
+        """ with open("jsonFile.json","r") as filepointer:
             if filepointer.read() == "":
                 with open("jsonFile.json", "w") as filepointer:
                     json.dump(dict, filepointer)
@@ -19,10 +36,17 @@ class JSONWriter:
                 else:
                     data.update(dict)
                     with open("jsonFile.json", "w") as filepointer:
-                        filepointer.write(json.dumps(data))
+                        filepointer.write(json.dumps(data))"""
 
     def delete_contact(self,number):
-        with open("jsonFile.json", "r") as filepointer:
+
+        if number in self.temp_dict.keys():
+            del self.temp_dict[number]
+        with open("jsonFile.json", "w") as filepointer:
+            filepointer.write(json.dumps(self.temp_dict))
+
+
+    """ with open("jsonFile.json", "r") as filepointer:
             if filepointer.read() == "":
                 print "Directory is empty"
             else:
@@ -36,10 +60,23 @@ class JSONWriter:
                         filepointer.write(json.dumps(data))
 
                 else:
-                    print "number not exist"
+                    print "number not exist" """
 
     def modify_contact(self, person):
-        with open("jsonFile.json", "r") as filepointer:
+        if person.get_phone_number() in self.temp_dict.keys():
+            x = self.temp_dict[person.get_phone_number()]
+            if (len(person.get_name()) != 0):
+                x["name"] = person.get_name()
+            if (len(person.get_city()) != 0):
+                x["city"] = person.get_city()
+            if (len(person.get_email_id()) != 0):
+                x["email"] = person.get_email_id()
+            y = {person.get_phone_number(): x}
+            self.temp_dict.update(y)
+            with open("jsonFile.json", "w") as filepointer:
+                filepointer.write(json.dumps(self.temp_dict))
+
+        """with open("jsonFile.json", "r") as filepointer:
             if filepointer.read() == "":
                 print "Directory is empty"
             else:
@@ -61,4 +98,4 @@ class JSONWriter:
                         filepointer.write(json.dumps(data))
 
                 else:
-                    print "number not exist"
+                    print "number not exist"""""
